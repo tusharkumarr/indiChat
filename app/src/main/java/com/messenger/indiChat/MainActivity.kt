93e7ee4c4dfe76c4c584ceabfef995f60e786cd8
Tushar Kumar <76609870+tusharkumarr@users.noreply.github.com>
2025-08-27 12:29:06 +0530

    refactor

--- app/src/main/java/com/messenger/indiChat/MainActivity.kt
+++ app/src/main/java/com/messenger/indiChat/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.messenger.indiChat.models.ChatMessage
 import android.widget.EditText
 import android.widget.ImageButton
+import com.messenger.indiChat.Adapter.ChatAdapter
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -61,38 +62,3 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-class ChatAdapter(private val messages: List<ChatMessage>) :
-    RecyclerView.Adapter<ChatAdapter.ChatViewHolder>() {
-
-    companion object {
-        private const val VIEW_TYPE_RECEIVED = 0
-        private const val VIEW_TYPE_SENT = 1
-    }
-
-    inner class ChatViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val messageText: TextView = itemView.findViewById(R.id.textMessage)
-        val timeText: TextView = itemView.findViewById(R.id.message_time)
-    }
-
-    override fun getItemViewType(position: Int): Int {
-        return if (messages[position].isSent) VIEW_TYPE_SENT else VIEW_TYPE_RECEIVED
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatViewHolder {
-        val layoutId = if (viewType == VIEW_TYPE_SENT) {
-            R.layout.item_message_sent
-        } else {
-            R.layout.item_message_received
-        }
-        val view = LayoutInflater.from(parent.context).inflate(layoutId, parent, false)
-        return ChatViewHolder(view)
-    }
-
-    override fun onBindViewHolder(holder: ChatViewHolder, position: Int) {
-        val msg = messages[position]
-        holder.messageText.text = msg.message
-        holder.timeText.text = msg.time
-    }
-
-    override fun getItemCount() = messages.size
-}
